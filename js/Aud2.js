c2230b491eed6f12972832b08b66a499e7e1fc9c
Ryan Merl <the.anti.9@gmail.com>
2011-08-13 17:03:34 -0400

    stupid git

--- js/Aud2.js
+++ js/Aud2.js
@@ -172,13 +172,6 @@ function audBindEvents() {
 			audPlayer.html('<div class="ui-state-error">Sorry, your browser does not support the HTML5 audio tag.</div>');
 		}
 	});
-	$('#filesubmit').click(function(event){
-		event.preventDefault();
-		if ($('#file').val()) {
-			ajaxFileUpload();
-		}
-		return false
-	});
 }
 
 function audNewSeeker() {
@@ -202,58 +195,6 @@ function audNewSeeker() {
 			audElem.seeking = false;
 		}
 	});
-}
-
-
-function ajaxFileUpload()
-{
-    //starting setting some animation when the ajax starts and completes
-    $("#loading")
-    .ajaxStart(function(){
-        $(this).show();
-    })
-    .ajaxComplete(function(){
-        $(this).hide();
-    });
-    
-    /*
-        prepareing ajax file upload
-        url: the url of script file handling the uploaded files
-                    fileElementId: the file type of input element id and it will be the index of  $_FILES Array()
-        dataType: it support json, xml
-        secureuri:use secure protocol
-        success: call back function when the ajax complete
-        error: callback function when the ajax failed
-        
-            */
-	$.ajaxFileUpload
-	(
-		{
-			url:'views/upload.php', 
-            secureuri:false,
-			fileElementId:'file',
-			dataType: 'json',
-			success: function (data, status)
-			{
-				if(typeof(data.error) != 'undefined')
-				{
-					if(data.error != '')
-					{
-						alert(data.error);
-					}else
-					{
-						alert(data.msg);
-					}
-				}
-			},
-			error: function (data, status, e)
-			{
-				alert(e);
-			}
-		}
-	)
-    
-	return false;
 }
 
 $(document).ready(function(){
