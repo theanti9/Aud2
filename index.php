859cb97c3dda889db1cda7ba41a20ef5ece312e2
Wil <wil@wilhall.com>
2011-08-14 16:46:50 -0400

    Added buffering bar + limits, started separating content into pages

--- index.php
+++ index.php
@@ -5,38 +5,51 @@
 		<link rel="stylesheet" type="text/css" href="css/cupertino/jquery-ui-1.8.15.custom.css" />
 	</head>
 	<body>
-		<div id="audPlayer">
-			<div id="audAudio">
-				<!-- HTML5 Audio Element -->
-			</div>
-			<div id="audNowMeta">
-				<span id="audCurArtist" class="audCont">Artist</span> - <span id="audCurAlbum" class="audCont">Album</span> - <span id="audCurSong" class="audCont">Song</span>
-			</div>
-			<div id="audControls">
-				<button id="audPrev">&lt;</button> <button id="audPlPa">Play</button> <button id="audNext">&gt;</button>
-				<br /><span class="audCont audTime" id="audTimePassed">00:00</span> <div class="audCont" id="audSeekCont"><div id="audSeek"></div></div> <span class="audCont audTime">-<span class="audCont" id="audTimeLeft">00:00</span></span><div class="audCont" id="audVolCont"><div id="audVol"></div></div>
+		<div class="audPage" id="audPageLogin">
+			<div id="audLogin">
+				<form action="#" method="POST">
+					<label>Username:</label><input type="text" name="username" />
+					<label>Username:</label><input type="password" name="password" />
+				</form>
 			</div>
 		</div>
-		<div id="audFileUpload">
-			<form action="views/upload.php" method="POST" id="audUpload" enctype="multipart/form-data">
-				<label>File: </label><input type="file" class="multi" name="file[]" accept="m4a|mp3|wav|ogg"/><img id="audLoading" src="img/loading.gif" /><br />
-				<input type="hidden" name="userid" value="1" />
-				<input type="hidden" name="upload_type" value="direct" />
-				<input type="submit" name="upload" />
-				<p>Music file or Zip file only.</p>
-			</form>
+		<div class="audPage" id="audPageLoading">
+			<div><p>Aud2 is loading...</p></div>
 		</div>
-		<div id="audLibrary">
-			<h3>Aud Library</h3>
-			<table>
-				<thead id="audLibHead">
-					<tr>
-						<th>#</th><th>Title</th><th>Artist</th><th>Album</th>
-					</tr>
-				</thead>
-				<tbody id="audLibBody">
-				</tbody>
-			</table>
+		<div class="audPage" id="audPageLoaded">
+			<div id="audPlayer">
+				<div id="audAudio">
+					<!-- HTML5 Audio Element -->
+				</div>
+				<div id="audNowMeta">
+					<span id="audCurArtist" class="audCont">Artist</span> - <span id="audCurAlbum" class="audCont">Album</span> - <span id="audCurSong" class="audCont">Song</span>
+				</div>
+				<div id="audControls">
+					<button id="audPrev">&lt;</button> <button id="audPlPa">Play</button> <button id="audNext">&gt;</button>
+					<br /><span class="audCont audTime" id="audTimePassed">00:00</span> <div class="audCont" id="audSeekCont"><div id="audSeek"></div></div> <span class="audCont audTime">-<span class="audCont" id="audTimeLeft">00:00</span></span><div class="audCont" id="audVolCont"><div id="audVol"></div></div>
+				</div>
+			</div>
+			<div id="audFileUpload">
+				<form action="views/upload.php" method="POST" id="audUpload" enctype="multipart/form-data">
+					<label>File: </label><input type="file" class="multi" name="file[]" accept="m4a|mp3|wav|ogg"/><img id="audLoading" src="img/loading.gif" /><br />
+					<input type="hidden" name="userid" value="1" />
+					<input type="hidden" name="upload_type" value="direct" />
+					<input type="submit" name="upload" />
+					<p>Music file or Zip file only.</p>
+				</form>
+			</div>
+			<div id="audLibrary">
+				<h3>Aud Library</h3>
+				<table>
+					<thead id="audLibHead">
+						<tr>
+							<th>#</th><th>Title</th><th>Artist</th><th>Album</th>
+						</tr>
+					</thead>
+					<tbody id="audLibBody">
+					</tbody>
+				</table>
+			</div>
 		</div>
 	</body>
 	<footer>
--- js/Aud2.js
+++ js/Aud2.js
@@ -1,16 +1,23 @@
-// Audio Player Controls, Containers, Info
+// Elements
 var audElem = null;
 var audAudio = null;
 var audPlayer = null;
 var audSeek = null;
+var audBuffer = null;
 var audPlPa = null;
-var seekPaused = null;
 var audTimePassed = null;
 var audTimeLeft = null;
+
+// Statistics
+var songSeconds = 0;
+var sessionSongs = 0;
+
+// Other Vars
 var lastValue = null;
+var seekPaused = null;
 var audioSupported = false;
-var mimesSupported = new Array();
-var mimesUnsupported = new Array();
+var mimesSupported = [];
+var mimesUnsupported = [];
 //// Useful audElem properties:
 // duration
 // currentTime
@@ -57,6 +64,12 @@ function updateTime(time, seek) {
 		time = Math.floor(audElem.currentTime);
 	}
 	// time is time we're currently at/seeking to
+
+	//Prevent seeking past buffer length
+	if(time >= audElem.buffered.end(0)) {
+		time = Math.floor(audElem.buffered.end(0)) - 1;
+	}
+
 	var passed = withMinutes(Math.floor(time));
 	var left = withMinutes(Math.floor(Math.floor(audElem.duration) - Math.floor(time)));
 	if(passed && left) {
@@ -66,6 +79,20 @@ function updateTime(time, seek) {
 		if(seek && audElem.currentTime != time) {
 			audElem.currentTime = time;
 		}
+	}
+}
+
+function saveStats(callback) {
+	if(songSeconds >= 0.80*audElem.duration) {
+		sessionSongs++;
+	}
+	songSeconds = 0;
+
+	// TODO: Send sessionSongs + songSeconds to PHP
+
+	sessionSongs = 0;
+	if (callback && typeof(callback) === "function") {
+		callback();
 	}
 }
 
@@ -82,6 +109,7 @@ function audSetup() {
 	audPlayer = $('#audPlayer');
 	audPlPa = $("#audPlPa");
 	audSeek = $('#audSeek');
+	audBuffer = $("#audBuffer");
 	audBindEvents();
 }
 
@@ -89,7 +117,7 @@ function audSetup() {
 function audSupportCheck() {
 	if(!!document.createElement('audio').canPlayType) {
 		audioSupported = true;
-		var mimes = ['audio/mpeg;', 'audio/ogg; codecs="vorbis"', 'audio/wav; codecs="1"', 'audio/mp4; codecs="mp4a.40.2"'];
+		var mimes = ['audio/mpeg;', 'audio/mp3;', 'audio/ogg; codecs="vorbis"', 'audio/wav; codecs="1"', 'audio/mp4; codecs="mp4a.40.2"'];
 		for(i=0;i<mimes.length;++i) {
 			if(!!(audElem.canPlayType(mimes[i]).replace(/no/, ''))) {
 				mimesSupported.push(mimes[i].substring(0, mimes[i].indexOf(';')));
@@ -105,7 +133,9 @@ function audSupportCheck() {
 }
 
 function audInit() {
-	audPlayer.fadeIn();
+	$("#audPageLoaded").fadeIn("slow", function(){
+		audPlayer.animate({'opacity': '1.0'});
+	});
 }
 
 function makeInitRequests() {
@@ -123,17 +153,20 @@ function makeInitRequests() {
 	});
 }
 
+//// Other
+//
 function changeSong(src) {
-	var mime = getMime(url);
-	if(mime in mimesSupported) {
-		audElem.pause();
-		audElem.src = src;
-		audElem.play();
-	}
-	else {
-		audAudio.append(['<div class="ui-state-error">Your browser does not support the audio type "', mime, '"</div>'].join(''))
-	}
-	
+	saveStats(function() {
+		var mime = getMime(url);
+		if(mime in mimesSupported) {
+			audElem.pause();
+			audElem.src = src;
+			audElem.play();
+		}
+		else {
+			audAudio.append(['<div class="ui-state-error">Your browser does not support the audio type "', mime, '"</div>'].join(''))
+		}
+	});
 }
 
 // Bind some JQuery + HTML5 event functions
@@ -165,6 +198,7 @@ function audBindEvents() {
 		changeSong(curPlayList[curSongIndex].songpath);
 	});
 
+	// Volume Slider
 	$("#audVol").slider({max: 1, step:0.01, value:0.5, orientation:'vertical', animate:'fast',
 		start: function(event, ui) {
 			audElem.volume = ui.value;
@@ -177,6 +211,7 @@ function audBindEvents() {
 		}
 	});
 
+	// Mousedown + Mouseup for seeking
 	$("#audSeekCont").mousedown(function(){
 		if(!audElem.paused) {
 			seekPaused = true;
@@ -194,6 +229,10 @@ function audBindEvents() {
 		}
 	})
 
+	// Save statistics before user leaves the page
+	window.onbeforeunload = saveStats();
+
+	// Initiate ajax form submit plugin
 	$("#audUpload").ajaxForm();
 	
 	//// HTML5 audio events
@@ -202,6 +241,7 @@ function audBindEvents() {
 		if(!audElem.seeking && !audElem.paused) {
 			updateTime();
 			$("#audSeek").slider("value", Math.floor(audElem.currentTime));
+			songSeconds++;
 		}
 	});
 
@@ -217,16 +257,38 @@ function audBindEvents() {
 			audPlayer.html('<div class="ui-state-error">Sorry, your browser does not support the HTML5 audio tag.</div>');
 		}
 	});
+
+	
+	$(audElem).bind("loadstart", function(){ // When the media starts loading
+		if(audElem.buffered != undefined) { // If browser supports .buffered
+			$(audElem).bind("progress", function(){ // Every time we add to the buffer
+				if(audElem.buffered.length != 0) {
+					audBuffer.animate({'width': [(audElem.buffered.end(0)/audElem.duration)*100, '%'].join('')});
+				}
+				else { // length == 0 if fully cached
+					audBuffer.animate({'width': '100%'});
+				}
+			});
+		}
+	});
 }
 
-function audNewSeeker() {
-	$("#audSeekCont").html('<div class="audCont" id="audSeek"></div>');
-	$('#audSeek').slider({value: 0.5, max: Math.floor(audElem.duration), animate:'fast',
+function audNewSeeker(repl) {
+	if(repl) {
+		$("#audSeekCont").html('<div class="audCont" id="audSeek"></div>');
+	}
+	audSeek.slider({value: 0.5, max: Math.floor(audElem.duration), animate:'fast',
 		start: function(event, ui) {
+			if(ui.value > audElem.buffered.end(0)) { // Prevent slide past buffer
+				return false;
+			}
 			// Set lastValue to where we currently are
 			lastValue = ui.value;
 		},
 		slide: function(event, ui) {
+			if(ui.value > audElem.buffered.end(0)) { // Prevent slide past buffer
+				return false;
+			}
 			// WhereWeAreNow - WhereWeWereLastMove = HowMuchWeMoved
 			var seekJump = ui.value - lastValue;
 			// Seek to WhereWeAreNow +- HowMuchWeMoved
@@ -234,16 +296,21 @@ function audNewSeeker() {
 			lastValue = ui.value;
 		},
 		stop: function(event, ui) {
+			if(ui.value > audElem.buffered.end(0)) { // Prevent slide past buffer
+				return false;
+			}
 			updateTime(ui.value, true);
 			lastValue = ui.value;
 			
 		}
 	});
+	audSeek.append('<span class="audCont" id="audBuffer"></span>');
+	audBuffer = $('#audBuffer');
 }
 
 $(document).ready(function(){
 	if($.browser.msie) {
-		window.location.href = "html/ie/html";
+		alert("IE Not Supported");
 	}
 	audSetup();
 });
